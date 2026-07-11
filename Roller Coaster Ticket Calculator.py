45f4ac5dccbd2c57a07c0ec33466d5b4f91955d5
Pritish <pritishk8280@gmail.com>
2026-07-11 17:26:26 +0530

    A dynamic claculator to calculate the price of a Roller Coaster ride base don the Age of the customer and whether they want a photo add-on or not. Also checks for the customer's height before proceeding to ask more questions.

--- Roller Coaster Ticket Calculator.py
+++ Roller Coaster Ticket Calculator.py
@@ -5,7 +5,7 @@ bill = 0
 
 
 if height >= 120:
-    print("You are allowed to ride the roller coaster.")
+    print("You are allowed to ride the Roller Coaster.")
     age = int(input("What is your Age? "))
     if age <= 12:
         bill = 5
@@ -15,15 +15,15 @@ if height >= 120:
         print("You have to pay $7.")
     elif 45 <= age <= 55:
         bill = 0
-        print("you have to pay $0.")
+        print("Ah! You're having a Midlife Crisis. The ride's on us.")
     else :
         bill = 12
         print("You have to pay $12.")
-    wants_photo = input("Do you wish to have a photo for $3extra? (y/n)")
+    wants_photo = input("Do you wish to have a photo for $3 extra? (y/n)")
     if wants_photo == "y":
         bill += 3
     print(f"You have to pay ${bill}.")
 
 else:
-    print("You are not allowed to ride the roller coaster.")
+    print("You are not allowed to ride the Roller Coaster.")
 
