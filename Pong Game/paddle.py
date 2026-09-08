920e09842be4c91da1e177a1d7dba93368acd0aa
Pritish <pritishk8280@gmail.com>
2026-09-08 12:36:43 +0530

    Pong Game

---
+++ Pong Game/paddle.py
@@ -0,0 +1,18 @@
+from turtle import Turtle, TNavigator
+
+class Paddle(Turtle):
+    def __init__(self, position):
+        super().__init__()
+        self.shape("square")
+        self.shapesize(5, 1)
+        self.color("white")
+        self.penup()
+        self.setposition(position)
+
+
+    def up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+    def down(self):
+        new_yx = self.ycor() - 20
+        self.goto(self.xcor(), new_yx)
---
+++ Pong Game/scoreboard.py
@@ -0,0 +1,42 @@
+from turtle import Turtle
+from turtle import Turtle
+ALIGNMENT ="center"
+FONT = ("Arial", 24, "normal")
+class ScoreBoard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.color("white")
+        self.penup()
+        self.hideturtle()
+        self.l_score = 0
+        self.r_score = 0
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.goto(-100, 180)
+        self.write(self.l_score, align="center", font=("courier", 80, "normal"))
+        self.goto(100, 180)
+        self.write(self.r_score, align="center", font=("courier", 80, "normal"))
+        self.speed("fastest")
+
+    def l_point(self):
+        self.l_score += 1
+        self.update_scoreboard()
+
+    def r_point(self):
+        self.r_score += 1
+        self.update_scoreboard()
+
+    #
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("GAME OVER ", align="center", font=FONT)
+    #
+    # def increase_score(self):
+    #     self.score += 1
+    #     self.clear()
+    #     self.update_scoreboard()
+
+
